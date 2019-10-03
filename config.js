d2fd0adcb4996fb70e65e8d76b830c63cf636a2b
anamika8 <anamikaeight@gmail.com>
2019-10-02 20:45:36 -0700

    changed config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/storyline-viewer-app";
+  process.env.DATABASE_URL || "mongodb://localhost/storyline-viewer-api";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
-  "mongodb://localhost/test-storyline-viewer-app";
+  "mongodb://localhost/test-storyline-viewer-api";
 exports.PORT = process.env.PORT || 8083;
